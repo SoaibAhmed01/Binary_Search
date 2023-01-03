45d2ff4811df9595fd6cbd5223388e45c2560344
SoaibAhmed217 <94387177+SoaibAhmed217@users.noreply.github.com>
2023-01-03 10:07:35 +0600

    Add files via upload

---
+++ codeslo.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++) cin>>arr[i];
+    int x;
+    cin>>x;
+    int ans=-1;
+    int low=0,high=n-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(arr[mid]==x)
+        {
+            ans=mid;
+            break;
+        }
+        else if(x<arr[mid])
+        {
+            high=mid-1;
+        }
+        else if(x>arr[mid])
+        {
+            low=mid+1;
+        }
+    }
+    cout<<ans<<endl;
+return 0;
+}
